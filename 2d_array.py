533a51ac11c01656b68a518ca41d2bb1d719dea6
Wasif Ali <199371745+Wasif-01@users.noreply.github.com>
2026-08-31 10:36:12 +0530

    1/9/26

--- 2d_array.py
+++ 2d_array.py
@@ -1,28 +1,66 @@
+
 marks = [
-    [80, 75, 90],   
-    [65, 85, 70],  
-    [90, 95, 88],   
-    [55, 60, 65],   
-    [72, 80, 78]    
+    [80, 75, 90],
+    [65, 88, 70],
+    [92, 78, 85],
+    [55, 60, 72],
+    [76, 95, 80]
 ]
 
+maximum = marks[0][0]
 
-maximum = max(max(row) for row in marks)
-print("Maximum marks:", maximum)
+for i in range(5):
+    for j in range(3):
+        if marks[i][j] > maximum:
+            maximum = marks[i][j]
+
+print("Maximum marks =", maximum)
 
 
-minimum = min(min(row) for row in marks)
-print("Minimum marks:", minimum)
+minimum = marks[0][0]
+
+for i in range(5):
+    for j in range(3):
+        if marks[i][j] < minimum:
+            minimum = marks[i][j]
+
+print("Minimum marks =", minimum)
 
 
-total = sum(sum(row) for row in marks)
-average = total / (5 * 3)
-print("Average marks:", average)
+total = 0
 
-subject_1 = [row[1] for row in marks]
-max_marks_subject_1 = max(subject_1)
+for i in range(5):
+    for j in range(3):
+        total = total + marks[i][j]
 
-student_id = subject_1.index(max_marks_subject_1)
+average = total / 15
 
-print("Maximum marks in subject 1:", max_marks_subject_1)
-print("Student ID:", student_id)
+print("Average marks =", average)
+
+
+maximum_subject1 = marks[0][0]
+student_id = 0
+
+for i in range(1, 5):
+    if marks[i][0] > maximum_subject1:
+        maximum_subject1 = marks[i][0]
+        student_id = i
+
+print("Student ID =", student_id)
+print("Maximum marks in Subject 1 =", maximum_subject1)
+
+
+
+for j in range(3):
+    subject_total = 0
+
+    for i in range(5):
+        subject_total = subject_total + marks[i][j]
+
+    subject_average = subject_total / 5
+
+    print("Average of Subject", j + 1, "=", subject_average)
+
+
+    minimum = marks[][2]
+    
